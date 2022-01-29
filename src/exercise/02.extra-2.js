26118e59dd183a90c5391070f2d7fdea49b6b3f0
Sheryl Hohman <SherylHohman@yahoo.com>
2022-01-28 22:33:44 -0800

    create empty extra-credit files
        So I do not have to restart the server every time.
        (The course instructional window needs the files to exist when the
          server is started via `npm start` in order to show tabs for the
          files, and links to the files, in the exercise window.
          If do not do this now, then have the restart the server everytime
          I create a new extra-credit exercise file. And that takes 4 minutes!
          This is not a hot-loading issue, it is because of the course interface.
    
          It may affect tests also. When tests exist for the extra-credit, I
          already told the test file to look for its corresponding extra-credit
          file in the exercises folder, so it is a good idea? to ensure the
          file I named actually exists!
        )
    
    ----
      On branch sh
      Changes to be committed:
     	new file:   src/exercise/01.extra-1.js
     	new file:   src/exercise/01.extra-2.js
     	new file:   src/exercise/01.extra-3.js
     	new file:   src/exercise/01.extra-4.js
     	new file:   src/exercise/02.extra-1.js
     	new file:   src/exercise/02.extra-2.js
     	new file:   src/exercise/02.extra-3.js
     	new file:   src/exercise/03.extra-1.js
     	new file:   src/exercise/06.extra-1.js

---
+++ src/exercise/02.extra-2.js
@@ -0,0 +1,4 @@
+// useCallback: custom hooks
+// 💯 return a memoized `run` function from useAsync
+// http://localhost:3000/isolated/exercise/02.extra-2.js
+
---
+++ src/exercise/06.extra-1.js
@@ -0,0 +1,4 @@
+// useDebugValue: useMedia
+// 💯 use the format function
+// http://localhost:3000/isolated/exercise/06.extra-1.js
+
