77c2986be739e907a8339cf1e416a95747829135
Sheryl Hohman <SherylHohman@yahoo.com>
2022-03-26 04:57:49 -0700

    01.extra-2: FIN | useReducer state now an object
    Refactor:
    useReducer stores counter as an object key {counter}
    rename count, setCount to state and setState
    ui now accesses count as state.count
    
    Because the usecase for useReducer is when multiple pieces of
    	inter-related state vars need to be updated at once, those
    	values are generally stored on some state object.
    	This is prep, I assume, for easing into old style reducers, where
    	state and actions (where action={type, payload}, and payload is
    		whatever properties need changed, or whatever data I neeed in order
    		to update the state appropriately. payload is usually dependant on
    		the action type. Different action types can require different data
    		sent via the payload object. (payload does not have to be an object;
    		it can be a regular value)
    This differs from useState, which typically handles a single value,
    	though it can also be an object of values, generally grouped together
    	because they all change at the same time.
    
    ----
    -	modified:   src/exercise/01.extra-2.js

--- src/exercise/01.extra-2.js
+++ src/exercise/01.extra-2.js
@@ -4,13 +4,16 @@
 
 import * as React from 'react'
 
-const countReducer = (state, step) => state + step
+// const countReducer = (state, step) => state + step
+const countReducer = (state, step) => ({count: state.count + step})
 
 function Counter({initialCount = 0, step = 1}) {
-  const [count, setCount] = React.useReducer(countReducer, initialCount)
+  const [state, setState] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
 
-  const increment = () => setCount(step)
-  return <button onClick={increment}>{count}</button>
+  const increment = () => setState(step)
+  return <button onClick={increment}>{state.count}</button>
 }
 
 function App() {
